5f5a2c24e036528ff8a36e84c13e3627b08873ae
Artsem Sauchuk <artsem.sauchuk@autenti.com>
2019-11-12 18:07:17 +0100

    hardcoded clientId

--- App.js
+++ App.js
@@ -2,14 +2,21 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import QRCode from 'react-native-qrcode';
 
+const clientId = 'hardcoded client id'
+
 export default function App() {
+
+  const qrCodeValue = {
+    clientId: clientId
+  }
+
   return (
     <View style={styles.container}>
       <QRCode
-          value='{"clientId":"some_client_id 2","businessId":"some_business_id 2"}'
-          size={200}
-          bgColor='purple'
-          fgColor='white'/>
+        value={JSON.stringify(qrCodeValue)}
+        size={200}
+        bgColor='purple'
+        fgColor='white' />
     </View>
   );
 }
